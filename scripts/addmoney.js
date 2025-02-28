1af710140844b7dd9109cba528a02679171c5b41
saim-alam1 <allbehassansaim@gmail.com>
2025-02-28 16:46:07 +0600

    Added The cash In feature

---
+++ scripts/addmoney.js
@@ -0,0 +1,24 @@
+document.getElementById('add-money-btn').addEventListener('click', function () {
+  const accountNumber = forValue('account-number');
+
+  const amountToAdd = convertToNumber('amount-number');
+
+  const pinNumber = convertToNumber('pin-number');
+
+  const mainBalance = convertMainBalanceIntoNumber('main-balance');
+
+  if (amountToAdd && pinNumber) {
+    if (accountNumber.length === 11) {
+      if (pinNumber === 1234) {
+        const sum = mainBalance + amountToAdd;
+        changeTheValue('main-balance', sum);
+      } else {
+        console.log('Enter a valid PIN');
+      }
+    } else {
+      console.log('Account number isn\'t valid');
+    }
+  } else {
+    console.log('Please enter all the inputs');
+  }
+})
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -6,7 +6,18 @@ function forValue(id) {
 // To convert into number
 function convertToNumber(id) {
   const input = document.getElementById(id).value;
-
   const convertedInput = parseInt(input);
   return convertedInput;
+}
+
+// To convert the main balance into number
+function convertMainBalanceIntoNumber(id) {
+  const input = document.getElementById(id).innerText;
+  const convertedInput = parseFloat(input);
+  return convertedInput;
+}
+
+// To change any values
+function changeTheValue(id, value) {
+  document.getElementById(id).innerText = value;
 }
